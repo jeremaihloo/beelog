300209beaeee77f3e5e3c3318331e3432c438867
jeremaihloo <jeremaihloo1024@gmail.com>
2020-07-25 01:34:07 +0800

    Update sample for log into file.

--- sample/main.go
+++ sample/main.go
@@ -4,5 +4,12 @@ import "github.com/jeremaihloo/beelog"
 
 func main() {
 	logger := beelog.NewLogger(1000)
-	logger.Info("Beelog !")
+	logger.EnableFuncCallDepth(true)
+	logger.SetLogger("console", "")
+	logger.SetLogger("file", `{"filename":"test.log","maxsize":10485760,"daily":true,"maxdays":10}`)
+	index := 0
+	for index < 10000 {
+		logger.Info("Beelog !")
+		index++
+	}
 }
